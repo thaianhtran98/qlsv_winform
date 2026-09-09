e4b3d74c1db10beb57c61adda4214b2b9be31d02
ttanh <ttanh@ctu.edu.vn>
2026-09-09 09:35:11 +0700

    fix

---
+++ QuanLySV/Helpers/GridViewHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySV.Helpers
+{
+	public class GridViewHelper
+	{
+		public void SetFixedColumn(DataGridViewColumn col, int width)
+		{
+			if (col == null) return;
+			col.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+			col.Width = width;
+			col.MinimumWidth = width;
+		}
+
+		public void SetFillColumn(DataGridViewColumn col, int minWidth, float fillWeight)
+		{
+			if (col == null) return;
+			col.MinimumWidth = minWidth;
+			col.FillWeight = fillWeight;
+			col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+		}
+	}
+}
